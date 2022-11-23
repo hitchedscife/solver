e11c183543b21002cbd6f3dda541e1e7ed7302c9
bob <95602665+hitchedscife@users.noreply.github.com>
2022-11-23 14:23:07 +0000

    Add files via upload

---
+++ solver.py
@@ -0,0 +1,21 @@
+from colorama import *
+
+def icon():
+    print(Fore.WHITE + """
+   _____
+    | |
+    | |
+   -----
+    | |   SOLVER INJECTOR
+    |""" + Fore.GREEN + "S" + Fore.WHITE + """|   
+    |""" + Fore.GREEN + "O" + Fore.WHITE + """|   -PC injection toolkit 
+    |""" + Fore.GREEN + "L" + Fore.WHITE + """|   -made for termux 
+    |""" + Fore.GREEN + "V" + Fore.WHITE + """|   -requires data cable
+    |""" + Fore.GREEN + "E" + Fore.WHITE + """|   -software made by Karkichan
+    |""" + Fore.GREEN + "R" + Fore.WHITE + """|
+    | |
+     V
+     |
+    """)
+
+icon()
